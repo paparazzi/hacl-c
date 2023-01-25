dd7c85bbe802921102e7a8fda0419a8b92fdce30
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2023-01-25 10:27:11 +0100

    Update kremlib.h
    fix compilation issue

--- kremlib.h
+++ kremlib.h
@@ -510,7 +510,7 @@ typedef FStar_UInt128_uint128 FStar_UInt128_t_, uint128_t;
 
 /* A series of definitions written using pointers. */
 static inline void print128_(const char *where, uint128_t *n) {
-  KRML_HOST_PRINTF("%s: [0x%08" PRIx64 ",0x%08" PRIx64 "]\n", where, n->high, n->low);
+  KRML_HOST_PRINTF("%s: [0x%08llx,0x%08llx]\n", where, n->high, n->low);
 }
 
 static inline void load128_le_(uint8_t *b, uint128_t *r) {
